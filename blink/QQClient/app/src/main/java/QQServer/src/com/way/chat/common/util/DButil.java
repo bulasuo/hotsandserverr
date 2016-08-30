d14e25cd0d9ebd69c91a64c3c8685a9cd4b1205b
bulasuo <bulasuo@foxmail.com>
2016-08-30 15:03:54 +0800

    add server

---
+++ blink/QQClient/app/src/main/java/QQServer/src/com/way/chat/common/util/DButil.java
@@ -0,0 +1,106 @@
+package com.way.chat.common.util;
+
+import java.io.FileNotFoundException;
+import java.io.IOException;
+import java.io.InputStream;
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.PreparedStatement;
+import java.sql.ResultSet;
+import java.sql.SQLException;
+import java.util.Properties;
+
+/**
+ * 数据库工具类
+ * 
+ * @author way
+ * 
+ */
+public class DButil {
+	/**
+	 * 连接数据库
+	 * 
+	 * @return 数据库连接对象
+	 */
+	public static Connection connect() {
+		Properties pro = new Properties();
+		String driver = null;
+		String url = null;
+		String username = null;
+		String password = null;
+		try {
+			InputStream is = DButil.class.getClassLoader()
+					.getResourceAsStream("DB.properties");
+			// System.out.println(is.toString());
+			pro.load(is);
+			driver = pro.getProperty("driver");
+			url = pro.getProperty("url");
+			username = pro.getProperty("username");
+			password = pro.getProperty("password");
+//			 System.out.println(driver + ":" + url + ":" + username + ":"
+//			 + password);
+			Class.forName(driver);
+			Connection conn = DriverManager.getConnection(url, username,
+					password);
+			return conn;
+		} catch (FileNotFoundException e) {
+			e.printStackTrace();
+		} catch (IOException e) {
+			e.printStackTrace();
+		} catch (ClassNotFoundException e) {
+			e.printStackTrace();
+		} catch (SQLException e) {
+			e.printStackTrace();
+		}
+		return null;
+	}
+	
+	/**
+	 * 关闭Connection
+	 * @param conn
+	 */
+	public static void close(Connection conn){
+		if(conn!=null){
+			try {
+				conn.close();
+			} catch (SQLException e) {
+				// TODO Auto-generated catch block
+				e.printStackTrace();
+			}
+		}
+	}
+	/**
+	 * 关闭PreparedStatement
+	 * @param sm
+	 */
+	public static void close(PreparedStatement  sm){
+		if(sm!=null){
+			try {
+				sm.close();
+			} catch (SQLException e) {
+				// TODO Auto-generated catch block
+				e.printStackTrace();
+			}
+		}
+	}
+	/**
+	 * 关闭ResultSet
+	 * @param rs
+	 */
+	public static void close(ResultSet rs){
+		if(rs!=null){
+			try {
+				rs.close();
+			} catch (SQLException e) {
+				// TODO Auto-generated catch block
+				e.printStackTrace();
+			}
+		}
+		
+	}
+
+//	public static void main(String[] args) {
+//		Connection con = new DButil().connect();
+//		System.out.println(con);
+//	}
+}
