ecdb0c18663adcd53c9b773c41072be9592856c7
bulasuo <bulasuo@foxmail.com>
2016-09-03 11:47:15 +0800

    9.3 ++

--- blink/javaTest/src/test/test.java
+++ blink/javaTest/src/test/test.java
@@ -8,12 +8,24 @@ public class test {
 	 * @param args
 	 */
 	public static void main(String[] args) {
-		byte b1 = (byte)11;
-		byte b2 = (byte)0xa1;
-		System.out.println(b1 == b2);
+//		byte b1 = (byte)11;
+		byte b2 = (byte)129;
+		int bb = 161;
+		System.out.println("::"+b2);
 		
-		final byte[] boundaryBytes = UUID.randomUUID().toString().getBytes();
-		System.out.println(boundaryBytes.length);
+//		for(int i = 0;i<11;i++)
+//			System.out.println(i);
+		
+		switch(bb){
+		case ((byte)0xa1)&0xff:
+			System.out.println("0xa1");
+			break;
+		case (byte)0xa2:
+			System.out.println("0xa2");
+			break;
+		}
+		
+		
 	}
 
 }
